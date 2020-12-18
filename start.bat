3fec45b6ba64da9a3f40753be28f224f86beeabf
ShengMin Shi <30469490+500kg@users.noreply.github.com>
2020-12-18 14:35:47 +0800

    Delete start.bat

--- start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-call C:\Github\ArknightsAutoHelper\start_base.bat 18016348242 Mr20191207 MB-7
-
-call C:\Github\ArknightsAutoHelper\start_base.bat 18116150131 Mr990202 MB-7
-
-rem call C:\Github\ArknightsAutoHelper\start_base.bat 13601460221 afa198816

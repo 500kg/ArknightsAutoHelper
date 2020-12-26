302f1490d13978ce07ea1a1c1a52dda13d0ceb2c
500kg <2426958134@qq.com>
2020-12-26 10:34:10 +0800

    2020/12/26

--- ArknightsHelper.py
+++ ArknightsHelper.py
@@ -13,6 +13,8 @@ main_handler 的启动模式
 from Arknights.helper import ArknightsHelper
 from collections import OrderedDict
 import argparse
+import win32gui
+import win32con
 
 def main():
     parser = argparse.ArgumentParser()
@@ -36,6 +38,7 @@ def main():
     #TASK_LIST['building'] = 1
     ep = config.get(user + '/c_id', '1-7')
     TASK_LIST[ep] = 3000
+    TASK_LIST['1-7'] = 3000 #削减体力至<7
     TASK_LIST['daily'] = config.get(user + '/daily', 'True')
 
     TASK_LIST['building'] = config.get(user + '/building', 'False')
@@ -44,6 +47,10 @@ def main():
     return Ark.main_handler(task_list=TASK_LIST, refill_with_item = refill_with_item)
 
 if __name__ == '__main__':
+    hwnd = win32gui.FindWindow(None,u"夜神模拟器") #返回窗口标题为Adobe Acrobat的句柄
+    left, top, right, bottom = win32gui.GetWindowRect(hwnd)
+    win32gui.SetWindowPos(hwnd, win32con.HWND_BOTTOM, 1839, 1064, right - left, bottom - top, win32con.SWP_SHOWWINDOW)
+
     sys.exit(main())
     
 
